ac6e2b8fb83c57b550e6b889ccb43f44c70738d6
Dadajon Jurakuziev <dadajonjurakuziev@gmail.com>
2018-11-27 02:18:56 +0900

    no message

--- CitiesRecyclerView/app/src/main/java/com/example/dadajonjurakuziev/citiesrecyclerview/ExperiencesViewAdapter.java
+++ CitiesRecyclerView/app/src/main/java/com/example/dadajonjurakuziev/citiesrecyclerview/ExperiencesViewAdapter.java
@@ -2,6 +2,7 @@ package com.example.dadajonjurakuziev.citiesrecyclerview;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
